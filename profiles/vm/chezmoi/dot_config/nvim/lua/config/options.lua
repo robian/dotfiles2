cba3a1551eafd510de368be49b696100910e8612
robian <288016+robian@users.noreply.github.com>
2026-05-28 10:52:23 +0200

    Fixes clipboard

--- profiles/vm/chezmoi/dot_config/nvim/lua/config/options.lua
+++ profiles/vm/chezmoi/dot_config/nvim/lua/config/options.lua
@@ -2,4 +2,19 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.undofile = false
-vim.g.clipboard = "osc52"
+vim.g.clipboard = {
+  name = "osc52-copy-only",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = function()
+      return { {}, "" }
+    end,
+    ["*"] = function()
+      return { {}, "" }
+    end,
+  },
+}
+vim.opt.clipboard = ""
